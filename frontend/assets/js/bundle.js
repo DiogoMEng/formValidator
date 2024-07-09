8a12ef6bf2444d4fdb271ff34e95d5c2d9979bf6
DIOGO DIAS MELLO <02843957222@trt8.jus.br>
2024-07-09 10:40:25 -0300

    :books: docs: atualizando readme.md

--- frontend/assets/js/bundle.js
+++ frontend/assets/js/bundle.js
@@ -83,19 +83,14 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = isByteLength;
-
 var _assertString = _interopRequireDefault(__webpack_require__(/*! ./util/assertString */ "./node_modules/validator/lib/util/assertString.js"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 /* eslint-disable prefer-rest-params */
 function isByteLength(str, options) {
   (0, _assertString.default)(str);
   var min;
   var max;
-
   if (_typeof(options) === 'object') {
     min = options.min || 0;
     max = options.max;
@@ -104,11 +99,9 @@ function isByteLength(str, options) {
     min = arguments[1];
     max = arguments[2];
   }
-
   var len = encodeURI(str).split(/%..|./).length - 1;
   return len >= min && (typeof max === 'undefined' || len <= max);
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -126,19 +119,12 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = isEmail;
-
 var _assertString = _interopRequireDefault(__webpack_require__(/*! ./util/assertString */ "./node_modules/validator/lib/util/assertString.js"));
-
 var _isByteLength = _interopRequireDefault(__webpack_require__(/*! ./isByteLength */ "./node_modules/validator/lib/isByteLength.js"));
-
 var _isFQDN = _interopRequireDefault(__webpack_require__(/*! ./isFQDN */ "./node_modules/validator/lib/isFQDN.js"));
-
 var _isIP = _interopRequireDefault(__webpack_require__(/*! ./isIP */ "./node_modules/validator/lib/isIP.js"));
-
 var _merge = _interopRequireDefault(__webpack_require__(/*! ./util/merge */ "./node_modules/validator/lib/util/merge.js"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var default_email_options = {
   allow_display_name: false,
   allow_underscores: false,
@@ -150,10 +136,9 @@ var default_email_options = {
   host_blacklist: [],
   host_whitelist: []
 };
+
 /* eslint-disable max-len */
-
 /* eslint-disable no-control-regex */
-
 var splitNameAddress = /^([^\x00-\x1F\x7F-\x9F\cX]+)</i;
 var emailUserPart = /^[a-z\d!#\$%&'\*\+\-\/=\?\^_`{\|}~]+$/i;
 var gmailUserPart = /^[a-z\d]+$/;
@@ -162,62 +147,55 @@ var emailUserUtf8Part = /^[a-z\d!#\$%&'\*\+\-\/=\?\^_`{\|}~\u00A1-\uD7FF\uF900-\
 var quotedEmailUserUtf8 = /^([\s\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|(\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*$/i;
 var defaultMaxEmailLength = 254;
 /* eslint-enable max-len */
-
 /* eslint-enable no-control-regex */
 
 /**
  * Validate display name according to the RFC2822: https://tools.ietf.org/html/rfc2822#appendix-A.1.2
  * @param {String} display_name
  */
-
 function validateDisplayName(display_name) {
-  var display_name_without_quotes = display_name.replace(/^"(.+)"$/, '$1'); // display name with only spaces is not valid
-
+  var display_name_without_quotes = display_name.replace(/^"(.+)"$/, '$1');
+  // display name with only spaces is not valid
   if (!display_name_without_quotes.trim()) {
     return false;
-  } // check whether display name contains illegal character
+  }
 
-
+  // check whether display name contains illegal character
   var contains_illegal = /[\.";<>]/.test(display_name_without_quotes);
-
   if (contains_illegal) {
     // if contains illegal characters,
     // must to be enclosed in double-quotes, otherwise it's not a valid display name
     if (display_name_without_quotes === display_name) {
       return false;
-    } // the quotes in display name must start with character symbol \
+    }
 
-
+    // the quotes in display name must start with character symbol \
     var all_start_with_back_slash = display_name_without_quotes.split('"').length === display_name_without_quotes.split('\\"').length;
-
     if (!all_start_with_back_slash) {
       return false;
     }
   }
-
   return true;
 }
-
 function isEmail(str, options) {
   (0, _assertString.default)(str);
   options = (0, _merge.default)(options, default_email_options);
-
   if (options.require_display_name || options.allow_display_name) {
     var display_email = str.match(splitNameAddress);
-
     if (display_email) {
-      var display_name = display_email[1]; // Remove display name and angle brackets to get email address
-      // Can be done in the regex but will introduce a ReDOS (See  #1597 for more info)
+      var display_name = display_email[1];
 
-      str = str.replace(display_name, '').replace(/(^<|>$)/g, ''); // sometimes need to trim the last space to get the display name
+      // Remove display name and angle brackets to get email address
+      // Can be done in the regex but will introduce a ReDOS (See  #1597 for more info)
+      str = str.replace(display_name, '').replace(/(^<|>$)/g, '');
+
+      // sometimes need to trim the last space to get the display name
       // because there may be a space between display name and email address
       // eg. myname <address@gmail.com>
       // the display name is `myname` instead of `myname `, so need to trim the last space
-
       if (display_name.endsWith(' ')) {
         display_name = display_name.slice(0, -1);
       }
-
       if (!validateDisplayName(display_name)) {
         return false;
       }
@@ -225,25 +203,19 @@ function isEmail(str, options) {
       return false;
     }
   }
-
   if (!options.ignore_max_length && str.length > defaultMaxEmailLength) {
     return false;
   }
-
   var parts = str.split('@');
   var domain = parts.pop();
   var lower_domain = domain.toLowerCase();
-
   if (options.host_blacklist.includes(lower_domain)) {
     return false;
   }
-
   if (options.host_whitelist.length > 0 && !options.host_whitelist.includes(lower_domain)) {
     return false;
   }
-
   var user = parts.join('@');
-
   if (options.domain_specific_validation && (lower_domain === 'gmail.com' || lower_domain === 'googlemail.com')) {
     /*
       Previously we removed dots for gmail addresses before validating.
@@ -252,26 +224,25 @@ function isEmail(str, options) {
       Gmail only normalizes single dots, removing them from here is pointless,
       should be done in normalizeEmail
     */
-    user = user.toLowerCase(); // Removing sub-address from username before gmail validation
+    user = user.toLowerCase();
 
-    var username = user.split('+')[0]; // Dots are not included in gmail length restriction
+    // Removing sub-address from username before gmail validation
+    var username = user.split('+')[0];
 
+    // Dots are not included in gmail length restriction
     if (!(0, _isByteLength.default)(username.replace(/\./g, ''), {
       min: 6,
       max: 30
     })) {
       return false;
     }
-
     var _user_parts = username.split('.');
-
     for (var i = 0; i < _user_parts.length; i++) {
       if (!gmailUserPart.test(_user_parts[i])) {
         return false;
       }
     }
   }
-
   if (options.ignore_max_length === false && (!(0, _isByteLength.default)(user, {
     max: 64
   }) || !(0, _isByteLength.default)(domain, {
@@ -279,7 +250,6 @@ function isEmail(str, options) {
   }))) {
     return false;
   }
-
   if (!(0, _isFQDN.default)(domain, {
     require_tld: options.require_tld,
     ignore_max_length: options.ignore_max_length,
@@ -288,41 +258,32 @@ function isEmail(str, options) {
     if (!options.allow_ip_domain) {
       return false;
     }
-
     if (!(0, _isIP.default)(domain)) {
       if (!domain.startsWith('[') || !domain.endsWith(']')) {
         return false;
       }
-
       var noBracketdomain = domain.slice(1, -1);
-
       if (noBracketdomain.length === 0 || !(0, _isIP.default)(noBracketdomain)) {
         return false;
       }
     }
   }
-
   if (user[0] === '"') {
     user = user.slice(1, user.length - 1);
     return options.allow_utf8_local_part ? quotedEmailUserUtf8.test(user) : quotedEmailUser.test(user);
   }
-
   var pattern = options.allow_utf8_local_part ? emailUserUtf8Part : emailUserPart;
   var user_parts = user.split('.');
-
   for (var _i = 0; _i < user_parts.length; _i++) {
     if (!pattern.test(user_parts[_i])) {
       return false;
     }
   }
-
   if (options.blacklisted_chars) {
     if (user.search(new RegExp("[".concat(options.blacklisted_chars, "]+"), 'g')) !== -1) return false;
   }
-
   return true;
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -340,13 +301,9 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = isFQDN;
-
 var _assertString = _interopRequireDefault(__webpack_require__(/*! ./util/assertString */ "./node_modules/validator/lib/util/assertString.js"));
-
 var _merge = _interopRequireDefault(__webpack_require__(/*! ./util/merge */ "./node_modules/validator/lib/util/merge.js"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 var default_fqdn_options = {
   require_tld: true,
   allow_underscores: false,
@@ -355,73 +312,63 @@ var default_fqdn_options = {
   allow_wildcard: false,
   ignore_max_length: false
 };
-
 function isFQDN(str, options) {
   (0, _assertString.default)(str);
   options = (0, _merge.default)(options, default_fqdn_options);
-  /* Remove the optional trailing dot before checking validity */
 
+  /* Remove the optional trailing dot before checking validity */
   if (options.allow_trailing_dot && str[str.length - 1] === '.') {
     str = str.substring(0, str.length - 1);
   }
+
   /* Remove the optional wildcard before checking validity */
-
-
   if (options.allow_wildcard === true && str.indexOf('*.') === 0) {
     str = str.substring(2);
   }
-
   var parts = str.split('.');
   var tld = parts[parts.length - 1];
-
   if (options.require_tld) {
     // disallow fqdns without tld
     if (parts.length < 2) {
       return false;
     }
-
     if (!options.allow_numeric_tld && !/^([a-z\u00A1-\u00A8\u00AA-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}|xn[a-z0-9-]{2,})$/i.test(tld)) {
       return false;
-    } // disallow spaces
+    }
 
-
+    // disallow spaces
     if (/\s/.test(tld)) {
       return false;
     }
-  } // reject numeric TLDs
+  }
 
-
+  // reject numeric TLDs
   if (!options.allow_numeric_tld && /^\d+$/.test(tld)) {
     return false;
   }
-
   return parts.every(function (part) {
     if (part.length > 63 && !options.ignore_max_length) {
       return false;
     }
-
     if (!/^[a-z_\u00a1-\uffff0-9-]+$/i.test(part)) {
       return false;
-    } // disallow full-width chars
+    }
 
-
+    // disallow full-width chars
     if (/[\uff01-\uff5e]/.test(part)) {
       return false;
-    } // disallow parts starting or ending with hyphen
+    }
 
-
+    // disallow parts starting or ending with hyphen
     if (/^-|-$/.test(part)) {
       return false;
     }
-
     if (!options.allow_underscores && /_/.test(part)) {
       return false;
     }
-
     return true;
   });
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -439,11 +386,8 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = isIP;
-
 var _assertString = _interopRequireDefault(__webpack_require__(/*! ./util/assertString */ "./node_modules/validator/lib/util/assertString.js"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 /**
 11.3.  Examples
 
@@ -478,27 +422,21 @@ var IPv4AddressFormat = "(".concat(IPv4SegmentFormat, "[.]){3}").concat(IPv4Segm
 var IPv4AddressRegExp = new RegExp("^".concat(IPv4AddressFormat, "$"));
 var IPv6SegmentFormat = '(?:[0-9a-fA-F]{1,4})';
 var IPv6AddressRegExp = new RegExp('^(' + "(?:".concat(IPv6SegmentFormat, ":){7}(?:").concat(IPv6SegmentFormat, "|:)|") + "(?:".concat(IPv6SegmentFormat, ":){6}(?:").concat(IPv4AddressFormat, "|:").concat(IPv6SegmentFormat, "|:)|") + "(?:".concat(IPv6SegmentFormat, ":){5}(?::").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,2}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){4}(?:(:").concat(IPv6SegmentFormat, "){0,1}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,3}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){3}(?:(:").concat(IPv6SegmentFormat, "){0,2}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,4}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){2}(?:(:").concat(IPv6SegmentFormat, "){0,3}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,5}|:)|") + "(?:".concat(IPv6SegmentFormat, ":){1}(?:(:").concat(IPv6SegmentFormat, "){0,4}:").concat(IPv4AddressFormat, "|(:").concat(IPv6SegmentFormat, "){1,6}|:)|") + "(?::((?::".concat(IPv6SegmentFormat, "){0,5}:").concat(IPv4AddressFormat, "|(?::").concat(IPv6SegmentFormat, "){1,7}|:))") + ')(%[0-9a-zA-Z-.:]{1,})?$');
-
 function isIP(str) {
   var version = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
   (0, _assertString.default)(str);
   version = String(version);
-
   if (!version) {
     return isIP(str, 4) || isIP(str, 6);
   }
-
   if (version === '4') {
     return IPv4AddressRegExp.test(str);
   }
-
   if (version === '6') {
     return IPv6AddressRegExp.test(str);
   }
-
   return false;
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -516,20 +454,15 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = assertString;
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function assertString(input) {
   var isString = typeof input === 'string' || input instanceof String;
-
   if (!isString) {
     var invalidType = _typeof(input);
-
     if (input === null) invalidType = 'null';else if (invalidType === 'object') invalidType = input.constructor.name;
     throw new TypeError("Expected a string but received a ".concat(invalidType));
   }
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -547,20 +480,16 @@ Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
 exports["default"] = merge;
-
 function merge() {
   var obj = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var defaults = arguments.length > 1 ? arguments[1] : undefined;
-
   for (var key in defaults) {
     if (typeof obj[key] === 'undefined') {
       obj[key] = defaults[key];
     }
   }
-
   return obj;
 }
-
 module.exports = exports.default;
 module.exports["default"] = exports.default;
 
@@ -594,7 +523,7 @@ module.exports["default"] = exports.default;
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
 (() => {
 var exports = __webpack_exports__;
 /*!**********************!*\
--- readme.md
+++ readme.md
@@ -1,12 +1,27 @@
-# PROJETO - VALIDADOR DE FORMULÁRIOS
+# VALIDADOR DE FORMULÁRIOS
 
-## Informações do HTML
+## 📝 Descrição
 
-Classes importantes:
-- error-message - menssagens de erros que serão exibidas após inserções incorretas de dados no formulário.
+O validador de formulários, projetado para assegurar que todas as entradas do usuário sejam verificadas e validadas de forma eficaz. Utilizando TypeScript, manipulo o DOM para implementar as validações necessárias em tempo real, garantindo que os dados inseridos pelos usuários sejam precisos e seguros antes de serem enviados. Este projeto não apenas melhora a experiência do usuário ao fornecer feedback instantâneo sobre a validade das entradas, mas também ajuda a evitar erros comuns de preenchimento de formulários. Ao integrar esta ferramenta com diversas aplicações, posso garantir que os dados coletados sejam consistentes e corretos, facilitando o processo de coleta de informações e aumentando a eficiência dos sistemas que dependem desses dados.
 
-## Pacotes
+## 🛠️ Funcionalidades
 
-Validator - usado para realizar o validação de formulários.
-- instalação: npm i validator.
-- com declaration types: npm i @types/validator(recomendado).
+1. Entrada de Dados.
+2. Validação de Entradas.
+3. Feedback ao Usuário.
+
+## 💻 Tecnologias Utilizadas
+
+1. TypeScript.
+2. HTML.
+3. CSS.
+
+## 🔰 Inicialização
+
+1. npm run build:frontend - Gera arquivo bundle.
+2. npm run build:backend - Compilo arquivos para JavaScript.
+3. Live Server.
+
+## Status do Projeto
+
+<img alt="projectstatus" src="https://img.shields.io/badge/Status do Projeto-Aprimoramento-gre">
